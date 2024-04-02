4f5d357e1d55f7f6baa7b5c4388e67ca7f5a9940
Neglon <thomasfneylon@gmail.com>
2024-04-02 15:01:14 -0400

    started user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,48 @@
-
+const { User, Thought } = require('../models');
 
 
 // get all users
+const userController = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find()
+
+            return res.status(200).json(users);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 
 
 //get a single user
+    async getUser (req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
 
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
 
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 //create a user
+    async createUser (req, res) {
+        try {
+            const user = await User.create(req.body);
+
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
 
 //update a user
@@ -17,3 +50,7 @@
 
 
 //delete a user
+
+
+
+};
